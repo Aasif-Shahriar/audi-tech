ea05012b68a3d3dc570d3b3f9c02fb2a230fbc98
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-08-26 15:39:33 +0600

    Changed the title

--- components/some-static-section/SectionTwo.jsx
+++ components/some-static-section/SectionTwo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function SectionTwo() {
   return (
-    <section className="w-full py-10 bg-white">
+    <section className="w-full py-10">
       <Link href={'/products'}><div className="w-full h-[400px]">
         <Image src='https://res.cloudinary.com/dnh9rdh01/image/upload/v1756136386/bannera-3_henqzy.png' alt="section-two image" width={500} height={500} className="w-full h-full object-cover"  />
       </div></Link>
